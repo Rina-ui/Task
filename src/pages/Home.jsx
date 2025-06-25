0288be16f76ba5f696477f7464e2d4621e47a946
Rina-ui <marinagracia895@gmail.com>
2025-06-25 15:40:28 +0200

    modification de la page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { use } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
 
 function Home() {
-    
+
+    const navigate = useNavigate();
+
+    const handleAddTask = () => {
+        navigate('../components/TaskForm.jsx');
+    }
+
+    return (
+        <div>
+            <h1>Welcome to the Task Manager</h1>
+            <p>This is a simple task management application.</p>
+            <button onClick={handleAddTask}>Ajouter une tache</button>
+            <button type="submit">Voir les taches</button>
+            <button type="submit">Voir la liste des taches</button>
+            <Link to="/theme">Theme</Link>
+        </div>
+    );
 }
 
 export default Home;
