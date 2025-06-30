42b602c6b20ae80228a8dfc54119feb60ae54805
Rina-ui <marinagracia895@gmail.com>
2025-06-30 13:07:20 +0200

    user route in app.js

--- BackExpress/app.js
+++ BackExpress/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.js';
 
 const app = express();
 
@@ -20,5 +21,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+app.use('/api/user',userRoutes);
 
 export default app; 
