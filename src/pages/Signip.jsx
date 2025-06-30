cc4bda8846036c7ea056a0448d16046a4577d6c8
Rina-ui <marinagracia895@gmail.com>
2025-06-30 14:06:25 +0200

    connecting to bsck

--- src/pages/Signip.jsx
+++ src/pages/Signip.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 
 function Signip(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await axios.post('http://localhost:3000/api/auth/signup', {
+                email,
+                password,
+
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error("Error during signup:", error);
+            alert("An error occurred during signup. Please try again.");
+        }
+    }
+
    return(
     <div>
         <h1>Sign Up</h1>
         <fieldset>
             <legend>Sign Up Form</legend>
-            <form>
+            <form onSubmit={handleSubmit}> 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" required />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <br />
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" required />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <br />
 
                 <button type="submit">Sign Up</button>
