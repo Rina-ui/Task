2ec068e73fb3781a280178dbb27b4e9de9c986e6
Rina-ui <marinagracia895@gmail.com>
2025-07-01 18:59:34 +0200

    rectification du usenavigate dans sign

--- src/pages/Signip.jsx
+++ src/pages/Signip.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useNavigate } from "react";
+import React, { useState} from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
